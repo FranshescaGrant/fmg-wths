f935607164b6bf7d3aaf3949711814a4bdce136e
FranshescaGrant <franshesca.grant@gmail.com>
2014-09-18 09:38:37 -0400

    All Decomposed

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,24 +1,32 @@
 public class DrawFigures1 {
 public static void main(String[] args) {
-<!--This is a comment/reminder to decompose this file-->
-        System.out.println("   /\\");
+//<!--This is a comment/reminder to decompose this file-->
+
+	down();
+	 System.out.println();
+	down();
+	up();
+	 System.out.println();
+	up();
+	flag();
+	up();
+
+       
+  
+    }
+	
+	public static void up() {
+		System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
-        System.out.println(" \\    /");
+	}
+	public static void down() {
+	    System.out.println(" \\    /");
         System.out.println("  \\  /");
         System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
+	}
+	public static void flag() {
+	    System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
@@ -28,8 +36,5 @@ public static void main(String[] args) {
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-    }
+	}
 }
